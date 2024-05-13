f3795fd1b5fde102dad2a6f1cc8e874c63db0d18
Elyticus <125762622+Elyticus@users.noreply.github.com>
2024-05-13 21:18:03 +0300

    image convertion

--- data.js
+++ data.js
@@ -1,76 +1,76 @@
 export const data = [
   {
     id: 1,
-    src: "./images/6.jpg",
+    src: "./images/6.webp",
   },
 
   {
     id: 2,
-    src: "./images/8.jpg",
+    src: "./images/8_1.webp",
   },
 
   {
     id: 3,
-    src: "./images/Unbroken V2.jpg",
+    src: "./images/Unbroken-V2.webp",
   },
 
   {
     id: 4,
-    src: "./images/6.jpg",
+    src: "./images/6.webp",
   },
 
   {
     id: 5,
-    src: "./images/8.jpg",
+    src: "./images/8_1.webp",
   },
 
   {
     id: 6,
-    src: "./images/Unbroken V2.jpg",
+    src: "./images/Unbroken-V2.webp",
   },
 
   {
     id: 7,
-    src: "./images/6.jpg",
+    src: "./images/6.webp",
   },
 
   {
     id: 8,
-    src: "./images/8.jpg",
+    src: "./images/8_1.webp",
   },
 
   {
     id: 9,
-    src: "./images/Unbroken V2.jpg",
+    src: "./images/Unbroken-V2.webp",
   },
 
   {
     id: 10,
-    src: "./images/6.jpg",
+    src: "./images/6.webp",
   },
 
   {
     id: 11,
-    src: "./images/8.jpg",
+    src: "./images/8_1.webp",
   },
 
   {
     id: 12,
-    src: "./images/Unbroken V2.jpg",
+    src: "./images/Unbroken-V2.webp",
   },
 
   {
     id: 13,
-    src: "./images/6.jpg",
+    src: "./images/6.webp",
   },
 
   {
     id: 14,
-    src: "./images/8.jpg",
+    src: "./images/8_1.webp",
   },
 
   {
     id: 15,
-    src: "./images/Unbroken V2.jpg",
+    src: "./images/Unbroken-V2.webp",
   },
 ];
